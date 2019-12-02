6453fd845c1ae245ce64d53b318b598deab56d64
Remco Poortinga - van Wijnen <remco.poortinga@surfnet.nl>
2019-12-02 10:30:39 +0100

    changed to using pandas dataframes for processing xlsx files (instead of processing .csv files)

---
+++ result-to-xlsx.py
@@ -0,0 +1,163 @@
+#! /usr/bin/env python3
+
+import sys
+import os
+import csv
+import requests
+import json
+import datetime
+import time
+import pandas as pd
+import numpy as np
+import math
+import pprint
+
+def openJSON(filename):
+    """open the JSON (result) file and return it as a json structure"""
+    data = {}
+    with open(filename) as f:
+        data = json.load(f)
+    return data
+
+def getMeasurementType(domains):
+    """Determine wether measurement results are for web or mail"""
+    measurementType = 'web'
+    status = 'failed'
+    for testDomain in domains:
+        if (testDomain['status'] == 'ok'):
+            for category in testDomain['categories']:
+                if (category['category'] == 'auth'):
+                    measurementType = 'mail'
+            break
+
+    return measurementType
+
+
+def JSONtoDF(data, domains_metadata, columns_to_add=['type']):
+    """Rework the JSON results to a DataFrame with the added metadata from the domains file"""
+
+    pp = pprint.PrettyPrinter(indent=4)
+
+    df = pd.DataFrame()
+    for cta in columns_to_add:
+        if cta in domains_metadata.columns:
+            df[cta] = np.nan
+        else:
+            print('Column \'{0}\' could not be found in domains metadata and will be ignored'.format(cta))
+
+    df['domain'] = ''
+    df['status'] = ''
+    df['score'] = int(0)
+
+    payload = data['data']
+    #    print('api-version: {}'.format(payload['api-version']))
+    #    print('name: {}'.format(payload['name']))
+    #    print('submission-date: {}'.format(payload['submission-date']))
+    #    print('finished-date: {}'.format(payload['finished-date']))
+
+    domains = payload['domains']
+
+    # Set index on pandas dataframe as the measurement type.
+    # That column contains the domains as used in the measurements
+    measurementType = getMeasurementType(domains)
+    domains_metadata = domains_metadata.set_index(measurementType, inplace=False)
+
+
+    for domainresults in domains:
+        domain = domainresults['domain']
+        df = df.append({'domain':domain}, ignore_index=True)
+
+        # add the additional metadata
+        if not domains_metadata.empty:
+            if domain in domains_metadata.index :
+                for md in columns_to_add:
+                    # check if metadata column exists first
+                    if md in domains_metadata.columns:
+                        df.iloc[-1, df.columns.get_loc(md)] = domains_metadata.at[domain, md]
+
+        if 'status' in domainresults:
+            df.iloc[-1, df.columns.get_loc('status')] = domainresults['status']
+
+        if 'score' in domainresults:
+            df.iloc[-1, df.columns.get_loc('score')] = int(domainresults['score'])
+
+        if 'categories' in domainresults:
+            dom_cats = domainresults['categories']
+            for cats in dom_cats:
+                # See if column already in resulting DataFrame
+                cat = cats['category']
+                if not cat in df.columns:
+                    df[cat] = np.nan
+
+                df.iloc[-1, df.columns.get_loc(cat)] = int(cats['passed'])
+
+        if 'views' in domainresults:
+            dom_views = domainresults['views']
+            for views in dom_views:
+                # See if column already in resulting DataFrame
+                view = views['name']
+                if not view in df.columns:
+                    df[view] = np.nan
+
+                df.iloc[-1, df.columns.get_loc(view)] = int(views['result'])
+
+        # Finally add the link as well
+        if not 'link' in df.columns:
+            df['link'] = np.nan
+
+        if 'link' in domainresults:
+            df.iloc[-1, df.columns.get_loc('link')] = domainresults['link']
+
+    return df
+
+#############################################
+
+pp = pprint.PrettyPrinter(indent=4)
+
+if len(sys.argv) < 2 :
+    print ('Usage: ')
+    print (sys.argv[0], ' <JSON results file|JSON results directory> [domains xlsx file] [metadata_column_name[,md_col_name2, ...]]')
+    quit(1)
+
+# Create empty dataframe
+domains = pd.DataFrame()
+domainsFile=''
+columns_to_add=['type']
+
+if len(sys.argv) > 2 :
+    domainsFile = sys.argv[2]
+
+if len(sys.argv) > 3 :
+    columns_to_add = sys.argv[3].split(',')
+
+filelist=[]
+filename = sys.argv[1]
+
+if os.path.isdir(filename):
+    if not filename.endswith("/"):
+        filename=filename+'/'
+    with os.scandir(filename) as it:
+        for entry in it:
+            if not entry.name.startswith('.') and entry.is_file() and entry.name.endswith('.json'):
+                    filelist.append('{0}{1}'.format(filename, entry.name))
+else:
+    filelist.append(filename)
+
+try:
+    if not domainsFile == '':
+        domains = pd.read_excel(domainsFile, sheet_name=0)
+except Exception as e:
+    print("error processing domains Excel file: {}".format(e))
+    exit(1)
+
+for fn in filelist:
+    print('Processing {0}'.format(fn))
+    data = openJSON(fn)
+    df = JSONtoDF(data, domains, columns_to_add)
+    df.sort_values(by=['score','domain'], ascending=False, inplace=True)
+    # Write the DataFrame to a file of the same name but with xlsx extension
+    fnr = fn[0:-5]+'.xlsx'
+    df.to_excel (fnr, index = None, header=True)
+
+
+# All done
